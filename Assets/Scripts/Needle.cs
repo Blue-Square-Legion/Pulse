330776413ee8ff5a3177919b263ace4a2280e27b
Robert Chaney <rwchaneyjr@yahoo.com>
2024-12-19 16:35:23 -0600

    update to ui
    still needs space bar for breaking

--- Assets/Scripts/Needle.cs
+++ Assets/Scripts/Needle.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Needle : MonoBehaviour
 {
-
+    [SerializeField]
+    TextMeshProUGUI _SteeringText, _SpeedText;
     public float value;
-    public GameObject needle;
-    
+    public GameObject Speedneedle;
+    public GameObject TurnNeedle;
     private float startPosition = 220f;
        private float endPosition;
     void Start()
@@ -17,20 +19,28 @@ public class Needle : MonoBehaviour
 
   public void  UpdateNeedle()
     {
-         value = Mathf.Sin(Time.deltaTime)*100f;
+       //  value = Mathf.Sin(Time.deltaTime)*100f;
        // var value = Mathf.Lerp(-1f, 1f, .02f);
       // float desiredPosition = startPosition - endPosition;
         //float temp = CarController.speed / 180;
       //  needle.transform.eulerAngles = new Vector3(0f, 0f,(startPosition - temp * desiredPosition));
-        needle.transform.eulerAngles = new Vector3(0f, 0f, SinMovementZ());
+     //   needle.transform.eulerAngles = new Vector3(0f, 0f, SinMovementZ());
     }
-    public float SinMovementZ()
+    public float SpeedSinMovementZ()
     {
-        return transform.position.z - Mathf.Sin(Time.time * 10) * 10 * Time.deltaTime;
+        return transform.position.z -  CarController.speed;
     }
-        void Update()
+
+    public float TurnDirSinMovementZ()
     {
-        value =800*(SinMovementZ());
-        needle.transform.eulerAngles = new Vector3(0f, 0f, value);
+        return transform.position.z - 80*CarController.steeringAxis;
+    }
+    void Update()
+    {
+        value =(SpeedSinMovementZ());
+        Speedneedle.transform.eulerAngles = new Vector3(0f, 0f, value+213f);
+      //  _SteeringText.text = CarController.steeringAxis.ToString("0.00");
+        _SpeedText.text =  CarController.speed.ToString("000.00");
+        TurnNeedle.transform.eulerAngles = new Vector3(0f, 0f, TurnDirSinMovementZ() - 83.5f); ;    
     }
 }
